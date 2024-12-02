794dd644679648eaacb6fee5f31885ce265251ff
Kazi Mohibuzzaman <kazimohib51@gmail.com>
2024-12-02 08:51:21 +0530

    did Q.6and 7,using functional way

--- recursion/script.js
+++ recursion/script.js
@@ -79,13 +79,51 @@
 
 // Q.6 print  N to 1 (without using (i-1) can use (i+1))-BACKTRACKING
 
-function print(i, n) {
-    if (i>n) {
-      return;
-    }
-    // console.log(i);
-    print(i+1, n);
-    process.stdout.write(i + " ");
+// function print(i, n) {
+//     if (i>n) {
+//       return;
+//     }
+//     // console.log(i);
+//     print(i+1, n);
+//     process.stdout.write(i + " ");
     
+//   }
+//   print(1,10);
+
+
+//**************** Lecutre-3 *************************
+  //------------------ Q.7 - Sum of first N numbers--------- (PARAMETERIZED WAY)---
+//   let n =5,s=0;
+//   function sum(i,s){
+//       if(i<1){
+//         console.log(`sum of ${n} till 1 is ${s}`)
+//         return;
+//       }
+//       sum(i-1,s+i);
+//   }
+// sum(n,s)
+
+// -----------------loop think from 1 to n --
+// let n =5,s=0 ;
+// let i=1;
+// function sum(i,n,s){
+//     if(i>n){
+//       console.log(`sum of ${n} till 1 is ${s}`)
+//       return;
+//     }
+//     sum(i+1,n,s+i);
+// }
+// sum(i,n,s)
+
+
+// ************ (FUNCTIONAL WAY) || (RECURSIVE WAY) **********
+let n =3;
+function sum(n){
+      if(n<1){
+        // console.log(`sum of ${n} till 1 is ${s}`)
+        return 0 ;
+      }
+     return (n + sum(n-1));
   }
-  print(1,10);
+  var ans = sum(n)
+console.log(`sum of n to 1 is ${ans}`);
