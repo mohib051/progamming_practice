3f33338a1c6ae9a8215e4aa87e5e49820618bd36
Kazi Mohibuzzaman <kazimohib51@gmail.com>
2024-12-12 02:01:02 +0530

    find largest element in the array

---
+++ arrays/script.js
@@ -0,0 +1,11 @@
+//Q.1 ---------------find the largest element in the array ---------
+
+// let arr= [2,13,5,1,14,10,25]
+// let max = arr[0]
+// for(let i =1 ; i<arr.length ; i++){
+//     if(arr[i]> max ){
+//         max = arr[i]
+//     } 
+// }
+// console.log(max)
+
