ecde1dce6d98d8822cc2c84edb713205c95d04c6
Kazi Mohibuzzaman <kazimohib51@gmail.com>
2024-12-11 18:47:01 +0530

    bubble sort algorithm

--- recursion/script.js
+++ recursion/script.js
@@ -302,3 +302,19 @@
 //       arr[i]=temp;
 // }
 // console.log(arr);
+
+
+//-----------------Bubble Sort--------------
+// let arr=[13,46,24,52,20,9]
+// let n=6;
+// for(let i = n-1 ; i>=1 ; i--){
+//   for(let j =0 ; j<=i-1 ; j++){
+//       if(arr[j] > arr[j+1]){
+//          let temp;
+//          temp= arr[j+1];
+//          arr[j+1] = arr[j]
+//          arr[j]= temp ;
+//       }  
+//   }
+// }
+// console.log(arr)
